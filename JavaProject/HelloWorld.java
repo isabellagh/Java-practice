88d5a373a0499974f4e95ed2ff668d09ce800302
isabellagh <isabellagh88@gmail.com>
2022-01-20 13:03:27 -0500

    short and long variables with max and min values

--- JavaProject/HelloWorld.java
+++ JavaProject/HelloWorld.java
@@ -26,5 +26,16 @@ public class HelloWorld {
         System.out.println("Byte Minimum Value = " + myMinByteValue);
         System.out.println("Byte Minimum Value = " + myMaxByteValue);
 
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Minimum Value = " + myMaxShortValue);
+
+
+        long myLongValue = 100L; 
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Minimum Value = " + myMaxLongValue);
     }
 }
