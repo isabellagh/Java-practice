6d03c20664b25c3b0dafdc626df4b8ea3c3f1b27
isabellagh <isabellagh88@gmail.com>
2022-01-20 12:49:16 -0500

    int variables values

--- JavaProject/HelloWorld.java
+++ JavaProject/HelloWorld.java
@@ -7,5 +7,17 @@ public class HelloWorld {
 
         int myFirstNumber = 5;
         System.out.println(myFirstNumber);
+
+        int myValue = 10000;
+
+        // Max and Min int values in Java
+        // Overflow and Underflow int values
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
     }
 }
