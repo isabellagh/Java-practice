58302640db9a8e1724347a6807055614af319956
isabellagh <isabellagh88@gmail.com>
2022-01-28 22:29:56 -0500

    char variables and unicode

--- JavaProject/HelloWorld.java
+++ JavaProject/HelloWorld.java
@@ -44,10 +44,21 @@ public class HelloWorld {
         System.out.println("Float Minimum Value = " + myMinFloatValue);
         System.out.println("Float Minimum Value = " + myMaxFloatValue);
 
-
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+        
         double myMinDoubleValue = Double.MIN_VALUE;
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double Minimum Value = " + myMinDoubleValue);
         System.out.println("Double Minimum Value = " + myMaxDoubleValue);
+
+
+        // Char is similar to string but it can only assign 1 character. 
+        // two bytes in memory
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
     }
 }
