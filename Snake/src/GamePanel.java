65d2263d9cf7f031eacf7ea9033aa4c9f2b66880
isabellagh <isabellagh88@gmail.com>
2022-02-05 14:20:44 -0500

    startGame method

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -31,12 +31,18 @@ public class GamePanel extends JPanel implements ActionListener{
         startGame();
     }
     public void startGame() {
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void paintComponent(Graphics g) {
 
     }
     public void draw(Graphics g) {
+
+    }
+    public void newApple(){
 
     }
     public void move() {
